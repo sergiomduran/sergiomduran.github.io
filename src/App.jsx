95132690cd198d5e90ab54dc2bacf6c51d478584
Sergio <alienfj92@gmail.com>
2025-09-08 19:45:56 +0200

    fix: add BrowserRouter for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './layouts/Layout.jsx';
 import Home from './pages/Home.jsx';
 import Projects from './pages/Projects.jsx';
@@ -10,16 +10,18 @@ import DopamineDetox from './pages/blog/DopamineDetox.jsx';
 export default function App() {
   console.log('[App] render');
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/writeups" element={<Writeups />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<Home />} />
-        <Route path="/blog/dopamine-detox" element={<DopamineDetox />} />
-      </Route>
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/writeups" element={<Writeups />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/blog/dopamine-detox" element={<DopamineDetox />} />
+          <Route path="*" element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
