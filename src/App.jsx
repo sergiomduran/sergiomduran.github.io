cda6f533bb2c3ebcce53cf53f39329953368f773
sergiomduran <alienfj92@gmail.com>
2026-07-28 19:14:39 +0200

    remove writeups

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Layout from './layouts/Layout.jsx';
 import Home from './pages/Home.jsx';
 import Projects from './pages/Projects.jsx';
 import Blog from './pages/Blog.jsx';
-import Writeups from './pages/Writeups.jsx';
 import Contact from './pages/Contact.jsx';
 import DopamineDetox from './pages/blog/DopamineDetox.jsx';
 import CVPage from './pages/blog/CVPage.jsx'; // 👈 añadido
@@ -15,7 +14,6 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/blog" element={<Blog />} />
-        <Route path="/writeups" element={<Writeups />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/blog/dopamine-detox" element={<DopamineDetox />} />
         <Route path="/blog/CVPage" element={<CVPage />} /> {/* 👈 tu CV */}
